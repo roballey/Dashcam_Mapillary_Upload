30ac087161a55b3c284a80a5d8ed2b0b4a758d4c
Rob Alley <rob.alley@gmail.com>
2026-04-05 16:09:35 +1200

    Add shell script to activate venv and run

---
+++ DashcamTransferAndUpload.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+# Activate venv and run script to transfer and upload dashcam files
+source ~/Projects/DashCam/Venv/bin/activate
+python3 ~/Projects/DashCam/Process_And_Upload_Dashcam.py
